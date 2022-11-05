b2440c5fed316663bf2901150a2abf0d3319aec8
zeus <zeuskeyz@gmail.com>
2022-11-05 11:14:44 +0300

    adding decoration to the submit button

--- js-files/app.js
+++ js-files/app.js
@@ -1,4 +1,4 @@
-const majibu = document.querySelectorAll("input[name='options']");
+const majibu = document.querySelectorAll("input[type='radio']");
 const maliza = document.querySelector("#finished");
 const matokeo = document.querySelector("#results");
 const alama = document.querySelector("#score");
@@ -6,28 +6,55 @@ const alama = document.querySelector("#score");
 let marks = 0;
 let totalMarks = 0;
 
+//adds all the possible values in the answer options to create a total
 majibu.forEach(function(jibu) {
 
     if(parseInt(jibu.value) > 0) {totalMarks += parseInt(jibu.value);}
     else{totalMarks = totalMarks;}
-
-    jibu.addEventListener("change",function(e){
-
-        marks += parseInt(e.target.value);
-    });   
-
 });
 
+//adds an event listener to the finish-quiz button 
 maliza.addEventListener("click",function(e){
 
-    let percentage = (marks/totalMarks)*100;
+    //loops through the answer options to check for the values of the selected options once the click event is fired and add them to the marks variable
+    majibu.forEach(function(jibu) {
+        if(jibu.checked){
+            marks += parseInt(jibu.value);
+        }
+    })    
+    
+    let percentage = (marks/totalMarks)*100; //turns the marks into percentage form
+    let level = "";
 
-    majibu.forEach(function(jibu){
 
-       jibu.setAttribute("disabled", "true");
+    if (percentage > 80){
+        level += "Excellently Passed";
+    }
 
+    else if(percentage >= 50 && percentage >= 80 ){
+        level += "Fairly Passed";
+    }
+
+    else{
+        level += "\nPoor performance, Please retake test.";
+
+    }
+    alama.textContent = "You have scored: " + percentage +"%"+level;
+    //percentage-=percentage; //prevents the submit button from continuing to double on subsequent clicks
+    //
+});
+
+
+$("#finished").click(function(){
+    
+    $("#results").removeClass("d-none");
+    $("#finished").text("QUIZ SUBMITTED!");
+    
+    majibu.forEach(function(jibu) {
+
+        $("input[type='radio']").attr("disabled",true)
+ 
     });
     
-    alama.textContent = "You have scored: " + percentage +"%";
-    matokeo.classList.remove("d-none");
 });
+
